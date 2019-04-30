333e5c9c44fcb43eb7a741970abef4e22fcf2828
Lee <leejin@c4b301d35313.ant.amazon.com>
2019-04-30 10:14:46 +0800

    sync

--- app/src/main/java/com/example/leejin/androidamplify/MainActivity.java
+++ app/src/main/java/com/example/leejin/androidamplify/MainActivity.java
@@ -3,11 +3,55 @@ package com.example.leejin.androidamplify;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.amazonaws.mobile.client.SignInUIOptions;
+import com.amazonaws.mobile.client.UserStateDetails;
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.Callback;
+import android.util.Log;
+import android.widget.TextView;
+
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AWSMobileClient.getInstance().initialize(getApplicationContext(), new Callback<UserStateDetails>() {
+            @Override
+            public void onResult(UserStateDetails userStateDetails) {
+                switch (userStateDetails.getUserState()){
+                    case SIGNED_IN:
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                TextView textView = (TextView) findViewById(R.id.text);
+                                textView.setText("Logged IN");
+                            }
+                        });
+                        break;
+                    case SIGNED_OUT:
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                TextView textView = (TextView) findViewById(R.id.text);
+                                textView.setText("Logged OUT");
+                            }
+                        });
+                        break;
+                    default:
+                        AWSMobileClient.getInstance().signOut();
+                        break;
+                }
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.e("INIT", e.toString());
+            }
+        });
+
     }
 }
